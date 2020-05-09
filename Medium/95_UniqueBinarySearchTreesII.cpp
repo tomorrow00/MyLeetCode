a8c5f67eff8a46be603038380845028bddcc7594
WangShang <shang.wang@zerotech.com>
2020-05-09 10:43:12 +0800

    complete 95

--- Medium/95_UniqueBinarySearchTreesII.cpp
+++ Medium/95_UniqueBinarySearchTreesII.cpp
@@ -21,13 +21,41 @@ using namespace std;
  * };
  */
 class UniqueBinarySearchTreesII {
+private:
+    vector<TreeNode*> helper(int from, int to) {
+        vector<TreeNode*> ret;
+
+        if (to - from < 0) ret.push_back(nullptr);
+        else if (to - from == 0) ret.push_back(new TreeNode(from));
+        else {
+            for(int i = from; i <= to; i++) {
+                auto lt = helper(from, i - 1);
+                auto rt = helper(i + 1, to);
+
+                for (int j = 0; j < lt.size(); ++j) {
+                    for (int k = 0; k < rt.size(); ++k) {
+                        auto m = new TreeNode(i);
+                        m->left = lt[j];
+                        m->right = rt[k];
+                        ret.push_back(m);
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
 public:
     vector<TreeNode*> generateTrees(int n) {
-
+        if (n == 0) return {};
+        return helper(1, n);
     }
 
     void main() {
-        int n = 3;
-        auto res = generateTrees(n);
+        int n = 0;
+        auto ret = generateTrees(n);
+        for (int i = 0; i < ret.size(); ++i) {
+            cout << ret[i]->val << endl;
+        }
     }
 };
