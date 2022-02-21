75756a9cfdc4ee9841a32652921eea89fe788a2e
tomorrow00 <yushang825@163.com>
2022-02-21 10:43:44 +0800

    451

---
+++ Medium/451_SortCharactersByFrequency.cpp
@@ -0,0 +1,41 @@
+//
+//  451_SortCharactersByFrequency.cpp
+//  LeetCode
+//
+//  Created by 王尚 on 2021/10/22.
+//  Copyright © 2021 tomorrow. All rights reserved.
+//
+
+#include "header.h"
+
+class SortCharactersByFrequency {
+public:
+    string frequencySort(string s) {
+        std::unordered_map<char, int> freq;
+        std::vector<string> bucket(s.size() + 1, "");
+        string res;
+
+        for (auto&& ch : s) {
+            freq[ch] ++;
+        }
+
+        for (auto&& frq : freq) {
+            char c = frq.first;
+            int n = frq.second;
+            bucket[n].append(n, c);
+        }
+
+        for (int i = s.size(); i > 0; i--) {
+            if (!bucket[i].empty()) {
+                res.append(bucket[i]);
+            }
+        }
+
+        return res;
+    }
+
+    void main() {
+        string s = "trere";
+        cout << frequencySort(s) << endl;
+    }
+};
