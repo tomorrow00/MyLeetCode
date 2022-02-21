d984076385cf0ec6f39488939439cd0319b83360
tomorrow00 <yushang825@163.com>
2022-02-21 10:44:06 +0800

    151

---
+++ Medium/151_ReverseWordsinaString.cpp
@@ -0,0 +1,35 @@
+//
+//  151_ReverseWordsinaString.cpp
+//  LeetCode
+//
+//  Created by 王尚 on 2021/10/20.
+//  Copyright © 2021 tomorrow. All rights reserved.
+//
+
+#include "header.h"
+
+class ReverseWordsinaString {
+public:
+    string reverseWords(string s) {
+        vector<string> vec;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            vec.push_back(word);
+        }
+        reverse(vec.begin(), vec.end());
+
+        string res;
+        for (const auto& v : vec) {
+            res += " " + v;
+        }
+        res.erase(0, 1);
+
+        return res;
+    }
+
+    void main() {
+        string str = "   the  sky is  blue";
+        cout << reverseWords(str) << endl;
+    }
+};
